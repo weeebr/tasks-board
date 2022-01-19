b453d43b5d911ab3c9e87e4a4e20176f93a6b4ec
Pascal Weber <p.weber.92@gmail.com>
2022-01-19 11:24:41 +0100

    update backup of my tasks

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -41,7 +41,7 @@ export class TaskService {
   }
 
   getTasks() {
-    return JSON.parse(localStorage.getItem('tasks')) || [];
+    return JSON.parse(localStorage.tasks) || [];
   }
 
   setTasks(tasks) {
